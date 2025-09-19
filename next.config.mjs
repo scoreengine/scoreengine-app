9d96c46d53ca2e7bcd94514ea90312e63bc4be9b
scoreengine <ben@scoreengine.io>
2025-09-19 15:47:33 +0200

    go

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  }
-};
-
+const nextConfig = { reactStrictMode: true };
 export default nextConfig;
