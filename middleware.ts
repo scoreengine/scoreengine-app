87d6a9121efa6408d05997a450fbd8b481026329
scoreengine <ben@scoreengine.io>
2025-09-19 17:11:01 +0200

    goooo

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-export default clerkMiddleware({
-  publicRoutes: ['/', '/api/health', '/api/lsz/webhook'],
-});
-export const config = { matcher: ['/((?!.*\\..*|_next).*)', '/'] };
